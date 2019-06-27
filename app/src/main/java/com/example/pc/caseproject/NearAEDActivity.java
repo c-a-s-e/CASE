eab4430595b8adb7ac1dc95d24a343b2bc10f74c
HSJ <dk07564@gmail.com>
2019-06-27 23:31:42 +0900

    xml textView

--- app/src/main/java/com/example/pc/caseproject/NearAEDActivity.java
+++ app/src/main/java/com/example/pc/caseproject/NearAEDActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -27,20 +29,31 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class NearAEDActivity extends FragmentActivity implements OnMapReadyCallback, AEDandSOScallUtil.APIListener {
     double mylatitude, mylongitude, aedlatitude, aedlongitude;
     private AED_FIND_REQUEST myRequest;
     private SupportMapFragment mapFragment;
-    String aedAddress;
-    TextView tv;
+    String aedAddress, nowAddress;
+    TextView aedTextView, myAddress, aedTextView2;
 
     @Override
     public void update() {
         mapFragment.getMapAsync(NearAEDActivity.this);
         aedAddress=myRequest.getAedAddress();
         Log.d("위치 ", aedAddress);
-        tv=findViewById(R.id.aedAddress);
-        tv.setText(aedAddress);
+
+        aedTextView=findViewById(R.id.address);
+        aedTextView.setText(aedAddress);
+
+        myAddress=findViewById(R.id.myAddress);
+        myAddress.setText(nowAddress);
+
+        aedTextView2=findViewById(R.id.aedAddress);
+        aedTextView2.setText(aedAddress);
     }
 
     @Override
@@ -56,6 +69,21 @@ public class NearAEDActivity extends FragmentActivity implements OnMapReadyCallb
         mylongitude=myLocation.getLongitude();
         myRequest.setMyLatitude(mylatitude);
         myRequest.setMyLongtitiude(mylongitude);
+
+        Geocoder mGeoCoder = new Geocoder(NearAEDActivity.this, Locale.KOREA);
+        List<Address> address;
+
+        try{
+            if(mGeoCoder !=null){
+                address=mGeoCoder.getFromLocation(mylatitude, mylongitude, 1);
+                if(address != null && address.size()>0){
+                    String currentLocationAddress = address.get(0).getAddressLine(0).toString();
+                    nowAddress = currentLocationAddress;
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
         AEDandSOScallUtil.getAEDdataFromAPI(this,myLocation,myRequest,false,true,this);
         mapFragment = (SupportMapFragment) (getSupportFragmentManager().findFragmentById(R.id.map));
