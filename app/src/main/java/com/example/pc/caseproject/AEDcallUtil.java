81f9c7e51927653dc197cc6d1dc15234a22430fd
Taeyoon Kim <tykimseoul@gmail.com>
2019-06-27 10:36:03 +0900

    code reformatting

--- app/src/main/java/com/example/pc/caseproject/AEDcallUtil.java
+++ app/src/main/java/com/example/pc/caseproject/AEDcallUtil.java
@@ -13,9 +13,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.firebase.iid.FirebaseInstanceId;
 
-import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -29,20 +27,20 @@ import javax.xml.parsers.DocumentBuilderFactory;
 public class AEDcallUtil {
     private static RequestQueue queue;
 
-    public static void getAEDdataFromAPIandSet (final Context context, final Location myLocation, final AED_FIND_REQUEST aed_find_request,
-                                                final boolean isSendPush, final boolean isNewActivity) {
+    public static void getAEDdataFromAPIandSet(final Context context, final Location myLocation, final AED_FIND_REQUEST aed_find_request,
+                                               final boolean isSendPush, final boolean isNewActivity) {
         //AED API로 콜해서 넣어서 보내기...
         String url = "http://apis.data.go.kr/B552657/AEDInfoInqireService/getAedLcinfoInqire?"
                 + "ServiceKey=h81QdjEyCaCY33uMnxkCku8XkhtY%2FZcgPxudUDzFlE7YCC%2BcUTm%2F1gBnVx9oz44IPUyteI8akUb8gQIuEwhbqg%3D%3D"
                 + "&WGS84_LON=" + myLocation.getLongitude() + "&WGS84_LAT=" + myLocation.getLatitude() + "&pageNum=1&numOfRows=1";
 
-        queue= Volley.newRequestQueue(context);
+        queue = Volley.newRequestQueue(context);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            response = new String(response.getBytes("ISO-8859-1"),"utf-8");
+                            response = new String(response.getBytes("ISO-8859-1"), "utf-8");
                             //((TextView)findViewById(R.id.textView)).setText(response);
 
                             InputSource is = new InputSource(new StringReader(response));
@@ -52,14 +50,14 @@ public class AEDcallUtil {
 
                             Element root = document.getDocumentElement();
                             NodeList myNL = root.getChildNodes(); //헤더랑 바디
-                            Element body = (Element)myNL.item(1);
+                            Element body = (Element) myNL.item(1);
                             myNL = (body).getChildNodes();
-                            Element items = (Element)myNL.item(0);
+                            Element items = (Element) myNL.item(0);
                             myNL = items.getChildNodes();
-                            Element item = (Element)myNL.item(0);
+                            Element item = (Element) myNL.item(0);
                             myNL = item.getChildNodes();
 
-                            aed_find_request.setAedAddress(myNL.item(12).getTextContent()+" "+
+                            aed_find_request.setAedAddress(myNL.item(12).getTextContent() + " " +
                                     myNL.item(5).getTextContent() + " " + myNL.item(0).getTextContent()
                                     + " " + myNL.item(10).getTextContent() + " " + myNL.item(1).getTextContent());
                             aed_find_request.setAedLatitude(Double.parseDouble(myNL.item(13).getTextContent()));
@@ -68,21 +66,21 @@ public class AEDcallUtil {
                             //SOS 요청은 isSendPush가 true 일때만 보낸다.
                             //if(isSendPush) sendPush(context, aed_find_request);
                             //주변 AED 찾기 일 경우에만
-                            if(isNewActivity){
-                                Toast.makeText(context,"이제 새 액티비티로 넘어갑니다", Toast.LENGTH_LONG).show();
-                                Intent intent = new Intent(context , NearAEDActivity.class);
-                                intent.putExtra("AED_find_request",aed_find_request);
+                            if (isNewActivity) {
+                                Toast.makeText(context, "이제 새 액티비티로 넘어갑니다", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(context, NearAEDActivity.class);
+                                intent.putExtra("AED_find_request", aed_find_request);
                                 context.startActivity(intent);
                             }
                         } catch (Exception e1) {
                             e1.printStackTrace();
-                            Log.d("parsing", "파싱 중 예외 발생"+e1.getMessage());
+                            Log.d("parsing", "파싱 중 예외 발생" + e1.getMessage());
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d("aed 정보","aed 정보 받아오는 중 오류 발생"+error.getMessage());
+                Log.d("aed 정보", "aed 정보 받아오는 중 오류 발생" + error.getMessage());
             }
         });
         stringRequest.setRetryPolicy
--- app/src/main/java/com/example/pc/caseproject/MainActivity.java
+++ app/src/main/java/com/example/pc/caseproject/MainActivity.java
@@ -2,42 +2,21 @@ package com.example.pc.caseproject;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-
-import java.io.StringReader;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RequestQueue queue;
-    private final static int LOCATION_REQ_CODE=1;
+    private final static int LOCATION_REQ_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,59 +26,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //주변 AED 찾기 버튼 누르면 실행될 메서드 입니다.
-    public void AEDFindButtonClicked(View v){
+    public void AEDFindButtonClicked(View v) {
         AED_FIND_REQUEST myAedRequest = new AED_FIND_REQUEST();
         Location myLocation = findMyLocation();
         myAedRequest.setMyLatitude(myLocation.getLatitude());
         myAedRequest.setAedLongtitude(myLocation.getLongitude());
         //이 메서드 안에서 자동으로 액티비티 넘어갑니다.
-        AEDcallUtil.getAEDdataFromAPIandSet(this,myLocation,myAedRequest,false,true);
+        AEDcallUtil.getAEDdataFromAPIandSet(this, myLocation, myAedRequest, false, true);
     }
 
 
-    public void getLocationPermission(){
+    public void getLocationPermission() {
         //위치권한 수용 요구하기
         int check = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-        if(check == PackageManager.PERMISSION_GRANTED) return; //수신 권한 이미 존재
-        else{
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
-                Toast.makeText(getApplicationContext(),"위치 권한을 허락해야 서비스 이용이 가능합니다.", Toast.LENGTH_LONG);
-            }
-            else{
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQ_CODE);
+        if (check == PackageManager.PERMISSION_GRANTED) return; //수신 권한 이미 존재
+        else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(getApplicationContext(), "위치 권한을 허락해야 서비스 이용이 가능합니다.", Toast.LENGTH_LONG);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQ_CODE);
             }
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode==LOCATION_REQ_CODE){
-            if(grantResults!=null && grantResults.length!=0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
-                Log.d("Permission","사용자가 권한 승인");
-            else Log.d("permission","사용자가 권한 거부");
+        if (requestCode == LOCATION_REQ_CODE) {
+            if (grantResults != null && grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Log.d("Permission", "사용자가 권한 승인");
+            else Log.d("permission", "사용자가 권한 거부");
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     //자신의 현재 위치를 파악하는 메서드 입니다.
-    public Location findMyLocation(){
+    public Location findMyLocation() {
         //**gps 기능이 켜졌는지 확인하는 코드가 필요합니다,
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=
-                PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
             getLocationPermission();
         }
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED) {
             LocationManager myLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             Location myLocation = myLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
             return myLocation;
-        }
-        else{
-            Toast.makeText(getApplicationContext(),"먼저 위치 권한을 확인해주세요",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "먼저 위치 권한을 확인해주세요", Toast.LENGTH_LONG).show();
             return null;
         }
     }
-
 
 
 }
