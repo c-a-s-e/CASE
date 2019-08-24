17180791497178e5c4b916f60acb1bcb922d4d56
Taeyoon Kim <tykimseoul@gmail.com>
2019-08-25 00:59:43 +0800

    design layout

--- app/src/main/java/com/example/pc/caseproject/ProviderActivity.kt
+++ app/src/main/java/com/example/pc/caseproject/ProviderActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toolbar
+import com.bumptech.glide.Glide
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,7 +21,7 @@ import kotlinx.android.synthetic.main.activity_provider.*
 import java.io.IOException
 import java.util.*
 
-class ProviderActivity : AppCompatActivity(), OnMapReadyCallback {
+class ProviderActivity : AppCompatActivity() {
     private val senderToken by lazy { intent.extras?.getString("sender-token") }
     private val senderLat by lazy { intent.extras?.getString("sender_latitude")?.toDouble() }
     private val senderLong by lazy { intent.extras?.getString("sender_longitude")?.toDouble() }
@@ -63,10 +64,7 @@ class ProviderActivity : AppCompatActivity(), OnMapReadyCallback {
             e.printStackTrace()
         }
 
-        val fm = supportFragmentManager
-        val f = fm.findFragmentById(R.id.map) as SupportMapFragment?
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this@ProviderActivity)
+        Glide.with(this).load(R.drawable.aed_location).into(location)
     }
 
     private fun showDialog() {
@@ -79,43 +77,5 @@ class ProviderActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
-    }
-
-    override fun onMapReady(map: GoogleMap) {
-        Log.e("got", aedLat.toString())
-        val sender = LatLng(senderLat!!, senderLong!!)
-        val sendermarkerOptions = MarkerOptions()
-        sendermarkerOptions.position(sender)
-
-        val bitmapdraw = resources.getDrawable(R.drawable.sendermarker) as BitmapDrawable
-        val bitmap = bitmapdraw.bitmap
-        val senderMarker = Bitmap.createScaledBitmap(bitmap, 104, 148, false)
-        sendermarkerOptions.icon(BitmapDescriptorFactory.fromBitmap(senderMarker))
-        map.addMarker(sendermarkerOptions)
-
-        val now = LatLng(myLat!!, myLong!!)
-        val mymarkerOptions = MarkerOptions()
-        mymarkerOptions.position(now)
-
-
-        val bitmapdraw2 = resources.getDrawable(R.drawable.mymarker) as BitmapDrawable
-        val bitmap2 = bitmapdraw2.bitmap
-        val myMarker = Bitmap.createScaledBitmap(bitmap2, 104, 148, false)
-        mymarkerOptions.icon(BitmapDescriptorFactory.fromBitmap(myMarker))
-        map.addMarker(mymarkerOptions)
-
-        val aed = LatLng(aedLat!!, aedLong!!)
-        val aedmarkerOptions = MarkerOptions()
-        aedmarkerOptions.position(aed)
-
-
-        val bitmapdraw3 = resources.getDrawable(R.drawable.aed) as BitmapDrawable
-        val bitmap3 = bitmapdraw3.bitmap
-        val aedMarker = Bitmap.createScaledBitmap(bitmap3, 88, 80, false)
-        aedmarkerOptions.icon(BitmapDescriptorFactory.fromBitmap(aedMarker))
-        map.addMarker(aedmarkerOptions)
-
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(now, 15f))
-
     }
 }
